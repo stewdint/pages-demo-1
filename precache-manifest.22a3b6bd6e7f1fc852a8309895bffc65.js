be53110db669505831946cc68c3a407ad6a28985
Lee Trout <lee@leetrout.com>
2019-04-23 22:47:14 -0400

    Add initial data

---
+++ precache-manifest.22a3b6bd6e7f1fc852a8309895bffc65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c0d6d6888cac8691386d",
+    "url": "/static/js/main.527ec1e7.chunk.js"
+  },
+  {
+    "revision": "3910e773f50de45c431f",
+    "url": "/static/js/2.64791022.chunk.js"
+  },
+  {
+    "revision": "c0d6d6888cac8691386d",
+    "url": "/static/css/main.b10f03eb.chunk.css"
+  },
+  {
+    "revision": "3910e773f50de45c431f",
+    "url": "/static/css/2.b361e481.chunk.css"
+  },
+  {
+    "revision": "21e5c7f0ccb82d68cc6ecac2340a28df",
+    "url": "/index.html"
+  }
+];
